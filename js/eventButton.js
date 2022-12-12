54ce9451c8499557dcdda67c8163a22d2d9c5500
Daniiil28 <117545645+Daniiil28@users.noreply.github.com>
2022-12-12 21:53:02 +0300

    Update eventButton.js

--- js/eventButton.js
+++ js/eventButton.js
@@ -1,7 +1,3 @@
 $('.js-buttons').mousedown(function () {
     $(this).addClass('buttons_scale ');
-}).click(function () {
-    return false;
-}).mouseup(function () {
-    $(this).removeClass('buttons_scale');
-});
+})
